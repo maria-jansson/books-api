c06fb8e509df115d1a6b25b41ada6409ee1d9951
Maria Jansson <mj225er@student.lnu.se>
2026-03-09 20:09:56 +0100

    Remove unused imports

--- src/main/java/com/github/maria_jansson/booksapi/dto/BookRequestDTO.java
+++ src/main/java/com/github/maria_jansson/booksapi/dto/BookRequestDTO.java
@@ -1,8 +1,5 @@
 package com.github.maria_jansson.booksapi.dto;
 
-import com.github.maria_jansson.booksapi.model.Author;
-import com.github.maria_jansson.booksapi.model.Category;
-
 import java.util.List;
 
 public record BookRequestDTO(
