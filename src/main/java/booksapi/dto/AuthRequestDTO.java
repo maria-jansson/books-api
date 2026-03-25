2fe00cc4d2f2f96d0c7bd70c0b10928859b35ca4
Maria Jansson <146979747+maria-jansson@users.noreply.github.com>
2026-03-25 12:14:22 +0100

    Add validation annotations to AuthRequestDTO fields

--- src/main/java/booksapi/dto/AuthRequestDTO.java
+++ src/main/java/booksapi/dto/AuthRequestDTO.java
@@ -6,4 +6,10 @@ package booksapi.dto;
  * @param username the username of the user
  * @param password the password of the user
  */
-public record AuthRequestDTO(String username, String password) {}
+public record AuthRequestDTO(
+  @NotBlank(message = "Username is required")
+  @Size(max = 50, message = "Username cannot exceed 50 characters")
+  String username, 
+  @NotBlank(message = "Password is required")
+  @Size(max = 100, message = "Password cannot exceed 100 characters")
+  String password) {}
